62deeba1f049a2133251e418f61bea9d7ffe308c
Vinicius <viniciuscarlos087@gmail.com>
2025-10-06 15:43:28 -0300

    fix: change a new styles to agroconecta site

--- frontend-web/src/utils/theme.ts
+++ frontend-web/src/utils/theme.ts
@@ -3,7 +3,7 @@ import { createTheme } from "@mui/material";
 export const theme = createTheme({
     palette: {
         primary: {
-            main: '#880022',
+            main: '#19CD94',
         },
     },
 });
